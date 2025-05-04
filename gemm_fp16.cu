72e29e30cbb2607f4274b6b628beb33775f53a32
Rishi Sankar <rishi.sankar@gmail.com>
2025-05-04 13:33:38 -0400

    gemm fp16 that works on tesla t4 too

--- gemm_fp16.cu
+++ gemm_fp16.cu
@@ -4,139 +4,101 @@
 
 using namespace nvcuda;
 
-constexpr int BW = 64;
-constexpr int NUM_WARPS = 4;
+constexpr int BM = 128, BK = 32, BN = 64;
+constexpr int MMA_WIDTH = 16; // assuming MMA multiplies 16x16
+constexpr int WM = 2, WN = 2; // width of each warp tile
+const int NUM_WARPTILES_X = BM / MMA_WIDTH / WM;
+const int NUM_WARPTILES_Y = BN / MMA_WIDTH / WN;
+constexpr int NUM_WARPS = NUM_WARPTILES_X * NUM_WARPTILES_Y;
 
-/*
-Approach:
-- uses tensor cores to multiply 16x16 matrices
-- 16 warps per block each handling a 16x16 region
-- blocks handle a 64x64 tile of output
-
-Only seems to work on H100 (TLE on the rest?). But it's the fastest solution on the site...
-Unsure why it TLEs on the others...
-*/
-
-template <bool isKEven>
 __global__ void matmul_kernel(const half* A, const half* B, half* C, int M, int N, int K, float alpha, float beta) {
-    __shared__ half SA[BW * BW];
-    __shared__ half SB[BW * BW];
-    __shared__ float SC[BW * BW];
-
-    float zero;
-    half *ptr_zero = reinterpret_cast<half*>(&zero);
-    ptr_zero[0] = __float2half(0.0f);
-    ptr_zero[1] = __float2half(0.0f);
-    float* SAfloat = reinterpret_cast<float *>(SA);
-    float* SBfloat = reinterpret_cast<float *>(SB);
-    const float* Afloat = reinterpret_cast<const float *>(A);
-    const float* Bfloat = reinterpret_cast<const float *>(B);
+    extern __shared__ half S[];
+    half *SA = S;  // size BM x BK
+    half *SB = S + BM * BK;  // size BK x BN
+    float *SC = reinterpret_cast<float*>(SB + BK * BN);  // size BM x BN
 
     int tid = threadIdx.x;
-    int warpId = threadIdx.x / 32;
-    int warpRow = warpId / 2;
-    int warpCol = warpId % 2;
+    int warpId = tid / 32;
+    int warpX = warpId / NUM_WARPTILES_Y;
+    int warpY = warpId % NUM_WARPTILES_Y;
 
-    int numBlocksN = (N+BW-1)/BW;
-    int numBlocksK = (K+BW-1)/BW;
-    
+    int numBlocksN = (N + BN - 1) / BN;
+    int numBlocksK = (K + BK - 1) / BK;
+
+    // each block computes a single block of output
     int blockId = blockIdx.x;
     int blockX = blockId / numBlocksN;
     int blockY = blockId % numBlocksN;
 
-    wmma::fragment<wmma::matrix_a, 16, 16, 16, half, wmma::row_major> a_frag1, a_frag2;
-    wmma::fragment<wmma::matrix_b, 16, 16, 16, half, wmma::row_major> b_frag1, b_frag2;
-    wmma::fragment<wmma::accumulator, 16, 16, 16, float> c_frag[4];
-    for (int i = 0; i < 4; i++) {
+    // initialize fragments (these will use register space)
+    wmma::fragment<wmma::matrix_a, 16, 16, 16, half, wmma::row_major> a_frag[WM];
+    wmma::fragment<wmma::matrix_b, 16, 16, 16, half, wmma::row_major> b_frag[WN];
+    wmma::fragment<wmma::accumulator, 16, 16, 16, float> c_frag[WM * WN];
+    for (int i = 0; i < WM * WN; i++) {
         wmma::fill_fragment(c_frag[i], 0.0f);
     }
 
     for (int iter = 0; iter < numBlocksK; iter++) {
-        // if k is even, able to load 2 halfs a time (20% speedup on nsight compute)
-        if constexpr (isKEven) {
-            // load block of A into shared memory
-            for (int i = tid; i < BW * BW / 2; i += blockDim.x) {
-                int row = i / (BW/2), col = i % (BW/2);
-                row += blockX * BW;
-                col += iter * (BW/2);
-                int Aidx = row * K / 2 + col;
-                SAfloat[i] = (Aidx >= M * K / 2) ? zero : Afloat[Aidx];
+        // load into SA
+        for (int i = tid; i < BM * BK; i += blockDim.x) {
+            int blockRow = i / BK, blockCol = i % BK;
+            int globalRow = blockRow + blockX * BM;
+            int globalCol = blockCol + iter * BK;
+            SA[i] = (globalRow >= M || globalCol >= K) ? __float2half(0.0f) : A[globalRow * K + globalCol];
+        }
+        // load into SB
+        for (int i = tid; i < BK * BN; i += blockDim.x) {
+            int blockRow = i / BN, blockCol = i % BN;
+            int globalRow = blockRow + iter * BK;
+            int globalCol = blockCol + blockY * BN;
+            SB[i] = (globalRow >= K || globalCol >= N) ? __float2half(0.0f) : B[globalRow * N + globalCol];
+        }
+        // sync threads to ensure all blocks have loaded their part
+        __syncthreads();
+
+        // do matmuls with tensor cores
+        for (int i = 0; i < BK / MMA_WIDTH; i++) {
+            for (int j = 0; j < WM; j++) {
+                wmma::load_matrix_sync(a_frag[j], SA + warpX * MMA_WIDTH * WM * BK + j * MMA_WIDTH * BK + i * MMA_WIDTH, BK);
             }
-            // load block of B into shared memory
-            for (int i = tid; i < BW * BW / 2; i += blockDim.x) {
-                int row = i / (BW/2), col = i % (BW/2);
-                row += iter * BW;
-                col += blockY * (BW/2);
-                int Bidx = row * N / 2 + col;
-                SBfloat[i] = (Bidx >= N * K / 2) ? zero : Bfloat[Bidx];
+            for (int j = 0; j < WN; j++) {
+                wmma::load_matrix_sync(b_frag[j], SB + warpY * WN * MMA_WIDTH + j * MMA_WIDTH + i * MMA_WIDTH * BN, BN);
             }
-        } else {
-            // load block of A into shared memory
-            for (int i = tid; i < BW * BW; i += blockDim.x) {
-                int row = i / BW, col = i % BW;
-                row += blockX * BW;
-                col += iter * BW;
-                SA[i] = (row >= M || col >= K) ? __float2half(0.0f) : A[row * K + col];
-            }
-            // load block of B into shared memory
-            for (int i = tid; i < BW * BW; i += blockDim.x) {
-                int row = i / BW, col = i % BW;
-                row += iter * BW;
-                col += blockY * BW;
-                SB[i] = (row >= K || col >= N) ? __float2half(0.0f) : B[row * N + col];
+            for (int j = 0; j < WM; j++) {
+                for (int k = 0; k < WN; k++) {
+                    wmma::mma_sync(c_frag[j * WN + k], a_frag[j], b_frag[k], c_frag[j * WN + k]);
+                }
             }
         }
-        // make sure all loads finished
-        __syncthreads();
-        
-        half *SA_warp_ptr1 = SA + 32 * BW * warpRow;
-        half *SA_warp_ptr2 = SA_warp_ptr1 + 16 * BW;
-        half *SB_warp_ptr1 = SB + 32 * warpCol;
-        half *SB_warp_ptr2 = SB_warp_ptr1 + 16;
-        for (int i = 0; i < 4; i++) {
-            wmma::load_matrix_sync(a_frag1, SA_warp_ptr1, BW);
-            wmma::load_matrix_sync(a_frag2, SA_warp_ptr2, BW);
-            wmma::load_matrix_sync(b_frag1, SB_warp_ptr1, BW);
-            wmma::load_matrix_sync(b_frag2, SB_warp_ptr2, BW);
 
-            wmma::mma_sync(c_frag[0], a_frag1, b_frag1, c_frag[0]);
-            wmma::mma_sync(c_frag[1], a_frag1, b_frag2, c_frag[1]);
-            wmma::mma_sync(c_frag[2], a_frag2, b_frag1, c_frag[2]);
-            wmma::mma_sync(c_frag[3], a_frag2, b_frag2, c_frag[3]);
-
-            SA_warp_ptr1 += 16;
-            SA_warp_ptr2 += 16;
-            SB_warp_ptr1 += 16 * BW;
-            SB_warp_ptr2 += 16 * BW;
+        for (int i = 0; i < WM; i++) {
+            for (int j = 0; j < WN; j++) {
+                int row = i + warpX * WM;
+                int col = j + warpY * WN;
+                wmma::store_matrix_sync(SC + (row * BN + col) * MMA_WIDTH, c_frag[i * WN + j], BN, wmma::mem_row_major);
+            }
         }
     }
 
-    float *SC_warp_ptr = SC + 32 * BW * warpRow + 32 * warpCol;
-    wmma::store_matrix_sync(SC_warp_ptr, c_frag[0], BW, wmma::mem_row_major);
-    wmma::store_matrix_sync(SC_warp_ptr + 16, c_frag[1], BW, wmma::mem_row_major);
-    wmma::store_matrix_sync(SC_warp_ptr + 16 * BW, c_frag[2], BW, wmma::mem_row_major);
-    wmma::store_matrix_sync(SC_warp_ptr + 16 * (BW+1), c_frag[3], BW, wmma::mem_row_major);
+    // sync threads to ensure all warps have stored their part
+    __syncthreads();
 
     // finally store back to global
-    __syncthreads();  // make sure all stores finished
-    for (int i = tid; i < BW * BW; i += blockDim.x) {
-        int row = i / BW;
-        int col = i % BW;
-        row += blockX * BW;
-        col += blockY * BW;
-        if (row < M && col < N) {
-            C[row * N + col] = __float2half(beta * __half2float(C[row * N + col]) + alpha * SC[i]);
+    for (int i = tid; i < BM * BN; i+= blockDim.x) {
+        int blockRow = i / BN, blockCol = i % BN;
+        int globalRow = blockRow + blockX * BM;
+        int globalCol = blockCol + blockY * BN;
+        if (globalRow < M && globalCol < N) {
+            C[globalRow * N + globalCol] = __float2half(beta * __half2float(C[globalRow * N + globalCol]) + alpha * SC[i]);
         }
     }
 }
 
 // A, B, and C are device pointers
 void solve(const half* A, const half* B, half* C, int M, int N, int K, float alpha, float beta) {
-    int numBlocksM = (M + BW -1) / BW;
-    int numBlocksN = (N + BW - 1) / BW;
-    if (K % 2 == 0) {
-        matmul_kernel<true><<<numBlocksM * numBlocksN, 32 * NUM_WARPS>>>(A, B, C, M, N, K, alpha, beta);
-    } else {
-        matmul_kernel<false><<<numBlocksM * numBlocksN, 32 * NUM_WARPS>>>(A, B, C, M, N, K, alpha, beta);
-    }
+    int numBlocksM = (M + BM - 1) / BM;
+    int numBlocksN = (N + BN - 1) / BN;
+    int shmemNeeded = (BM + BN) * BK * sizeof(half) + (BM * BN) * sizeof(float);
+    cudaFuncSetAttribute(matmul_kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, 98*1024);
+    matmul_kernel<<<numBlocksM * numBlocksN, 32 * NUM_WARPS, shmemNeeded>>>(A, B, C, M, N, K, alpha, beta);
 }
